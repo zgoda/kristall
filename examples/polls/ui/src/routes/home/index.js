967dfc4e264c1c50ee4d9aa6d07c30de436bc80c
Jarek Zgoda <jarek.zgoda@gmail.com>
2019-12-27 19:07:08 +0100

    Display poll list in UI

--- examples/polls/ui/src/routes/home/index.js
+++ examples/polls/ui/src/routes/home/index.js
@@ -1,11 +1,32 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import style from './style';
 
-const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-	</div>
-);
+class Home extends Component {
+  state = { polls: [] };
+
+  async componentDidMount() {
+    const url = '/api/polls';
+    const resp = await fetch(url);
+    const result = await resp.json();
+    this.setState({ polls: result });
+  };
+
+  render() {
+    const items = [];
+    for (const [index, poll] of this.state.polls.entries()) {
+      items.push(<li key={index}>{poll.title}</li>)
+    }
+    return (
+      <div class={style.home}>
+        <h1>Polls</h1>
+        <p>Current polls</p>
+        <ul>
+          {items}
+        </ul>
+      </div>
+    )
+  };
+
+};
 
 export default Home;
