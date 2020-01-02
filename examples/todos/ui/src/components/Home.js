2d02c7da19bba469fa2bd7c35fd6dbecae0b7d69
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-01-03 00:51:48 +0100

    I hate javascript

--- examples/todos/ui/src/components/Home.js
+++ examples/todos/ui/src/components/Home.js
@@ -7,30 +7,39 @@ class UserSelector extends Component {
     const url = '/api/user';
     fetch(url)
       .then(res => res.json())
-      .then(data => this.setState({ users: data || []}))
+      .then(data => this.setState({ users: data || []}));
   };
 
   shouldComponentUpdate() {
     return Boolean(this.state.users);
-  }
+  };
 
   onInput = (e) => {
-    this.setState({ value: e.target.value })
-  }
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.value);
+  };
 
   render(_, { value }) {
     return (
       <div>
-        <h2>Select user or create new</h2>
-        <select value={value} onInput={this.onInput}>
-          {this.state.users.map(user => (
-          <option key={user.pk} value={user.pk}>{user.name}</option>
-          ))}
-        </select>
+        <h2>Select user</h2>
+        <form onSubmit={this.handleSubmit}>
+          <select value={value} onInput={this.onInput}>
+            <option key="none" value="none">---</option>
+            {this.state.users.map(user => (
+            <option key={user.pk} value={user.pk}>{user.name}</option>
+            ))}
+          </select>
+          <button type="submit">select</button>
+        </form>
       </div>
     );
-  }
-}
+  };
+};
 
 const Home = () => (
 	<div>
