f290db72a2e0a89c8629a2eedbbfed58404d7b72
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-01-31 12:49:44 +0100

    Updated example with sqlite options

--- examples/todos/src/todos/models.py
+++ examples/todos/src/todos/models.py
@@ -21,5 +21,21 @@ class Todo(db.Entity):
     comment = Optional(str)
 
 
+db_pragmas = {
+    'journal_mode': 'wal',
+    'cache_size': -1 * 16000,
+    'foreign_keys': 1,
+    'ignore_check_constraints': 0,
+    'synchronous': 0
+}
+
+
+@db.on_connect(provider='sqlite')
+def set_pragmas(db, connection):
+    cursor = connection.cursor()
+    for name, value in db_pragmas.items():
+        cursor.execute(f'PRAGMA {name} = {value}')
+
+
 db.bind(provider='sqlite', filename=os.environ['TODOS_DB'], create_db=True)
 db.generate_mapping(create_tables=True)
